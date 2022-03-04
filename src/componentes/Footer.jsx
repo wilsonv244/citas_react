6e099b30712f723b3fb384bf7b42867d6c31cda2
wilsonv244 <39635463+wilsonv244@users.noreply.github.com>
2022-03-03 23:44:36 -0500

    algunos cambios

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -2,8 +2,8 @@ function Footer() {
     return(
         <>
             <div className="mt-72 bg-green-300 p-10 flex space-x-96 justify-center">
-                <h1 className="text-white">Seguimiento de pacientes</h1>    
-                <h1 className="text-white">Elaborado por <span className="font-bold">Wilson Vargas B</span></h1>    
+                <h1 className="text-white text-lg">Seguimiento de Pacientes</h1>    
+                <h1 className="text-white text-lg">Elaborado por <span className="font-bold">Wilson Vargas B</span></h1>    
             </div>            
         </>
     )
