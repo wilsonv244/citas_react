c385b2a1f80b2625ecbaea6aaf3ad36a86a49aef
wilsonv244 <39635463+wilsonv244@users.noreply.github.com>
2022-03-06 10:59:54 -0500

    probando las ramas de GIT

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return(
         <>
             <div className="mt-72 bg-green-300 p-10 flex space-x-96 justify-center">
-                <h1 className="text-white text-lg">Seguimiento de Pacientes</h1>    
+                <h1 className="text-white text-lg">Seguimiento de Pacientes <span className="bg-blue-300 font-black">Desde otra rama</span></h1>    
                 <h1 className="text-white text-lg">Elaborado por <span className="font-bold">Wilson Vargas B</span></h1>    
             </div>            
         </>
