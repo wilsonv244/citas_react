e628c45c5681de5a2ebdce4b2d7e8f9171520bab
wilsonv244 <39635463+wilsonv244@users.noreply.github.com>
2022-04-17 00:25:45 -0500

    texto editado

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return(
         <>
-            <h1 className="font-black text-5xl text-center text-gray-600 w-1/2 mx-auto md:w-2/3">Seguimiento de pacientes{' '}
+            <h1 className="md:ml-10 font-black text-3xl md:text-5xl text-center text-gray-600 w-1/2 mx-auto md:w-2/3">Seguimiento de pacientes{' '}
             <span className="text-indigo-500">Veterinaria</span></h1>
             
         </>
